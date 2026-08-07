6f5de66e5e45bd8c5c14a8b41775274b551fba6a
PROJOMINT <lunrfn1@gmail.com>
2026-08-07 17:45:53 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,37 @@
+--[[
+ __    __     __     __   __     ______  
+/\ "-./  \   /\ \   /\ "-.\ \   /\__  _\ 
+\ \ \-./\ \  \ \ \  \ \ \-.  \  \/_/\ \/ 
+ \ \_\ \ \_\  \ \_\  \ \_\\"\_\    \ \_\ 
+  \/_/  \/_/   \/_/   \/_/ \/_/     \/_/ 
+          LOADER                             
+]]--
+
+
+getgenv().PlaceList = {
+    [379614936] = "https://raw.githubusercontent.com/AERO-RBX/MINT/refs/heads/main/games/assassin", 
+    [286090429] = "https://raw.githubusercontent.com/AERO-RBX/MINT/refs/heads/main/games/arsenal"
+}
+
+
+local placeId = game.PlaceId
+getgenv().Import = getgenv().PlaceList[placeId]
+
+
+local MPS = game:GetService("MarketplaceService")
+
+local success, info = pcall(function()
+    return MPS:GetProductInfo(game.PlaceId)
+end)
+
+local placeName = success and info.Name or "Unknown Place"
+local gameName = success and game:GetService("MarketplaceService"):GetProductInfo(game.GameId).Name or "Unknown Game"
+
+
+if getgenv().Import then
+    print("Loading script for place:", placeName)
+    loadstring(game:HttpGet(getgenv().Import))()
+else
+    warn("No script available for this game.")
+end
+
